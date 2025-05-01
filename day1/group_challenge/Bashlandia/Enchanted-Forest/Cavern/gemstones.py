e7f3ec85a9e196dc98c9da748aa0846053e9a763
Liam Dubay <liam.dubay@gmail.com>
2025-05-01 15:39:21 -0400

    add gemstones task

---
+++ day1/group_challenge/Bashlandia/Enchanted-Forest/Cavern/gemstones.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import os
+
+torch = Path('torch.txt')
+gemstones = ['gemstone1.txt', 'gemstone2.txt', 'gemstone3.txt', 'gemstone4.txt', 'gemstone5.txt']
+
+if torch.is_file():
+    # Reveal gemstone files
+    for g in gemstones:
+        os.rename('.%s' % g, g)
+    print(
+"""
+Under the light of your torch, five gemstones glitter against the dark walls
+of the cavern. You think you should be able to find a stone for the witch here
+- but which one?
+
+Hint: use the man command to look up the documentation for ls. There is a flag
+that will list the size of a file next to its name. Remember that you're
+looking for a file that's exactly 85 bytes.
+"""
+    )
+else:
+    if all([g in os.listdir() for g in gemstones]):
+        # Hide gemstone files
+        for g in gemstones:
+            os.rename(g, '.%s' % g)
+    print(
+"""
+You turn a corner and are greeted with pitch darkness. You can't even see your
+own hand in front of your face, let alone search for these gemstones. You 
+probably should have lit your own torch using the torches at the cave's 
+entrance.
+
+Hint: copy the torch.txt file from the previous directory into this one.
+"""
+    )
--- day1/group_challenge/Bashlandia/Enchanted-Forest/Swamplands/hut.py
+++ day1/group_challenge/Bashlandia/Enchanted-Forest/Swamplands/hut.py
@@ -2,10 +2,88 @@ from pathlib import Path
 import os
 
 cauldron = Path('Cauldron/')
-ingredients = ['dragonscale', 'flamecap', 'finger']
+ingredients = ['dragonscale', 'flamecap', 'gemstone4']
+gemstone = cauldron / ('%s.txt' % ingredients[-1])
 clue = Path('clue.txt')
 
-if not cauldron.is_dir():
+if cauldron.is_dir():
+    if any([g in os.listdir(cauldron) for g in ['gemstone1.txt', 'gemstone2.txt', 'gemstone3.txt', 'gemstone5.txt']]):
+        # Player has provided the wrong gemstone
+        print(
+"""
+The cauldron bubbles intensely for a moment. Suddenly, the gemstone you tossed
+in comes hurtling back out toward you. It smacks you in the forehead. Seems
+like you found the wrong stone!
+"""
+        )
+    elif all([(cauldron / ('%s.txt' % i)).is_file() for i in ingredients]):
+        if clue.is_file():
+            # Witch has already given the clue
+            print(
+"""
+Try as you might, you cannot find the clearing where you encountered the
+witch before. Something tells you this isn't the last you've seen of her...
+"""
+            )
+        else:
+            if len(os.listdir(cauldron)) == len(ingredients):
+                # Player has found all the ingredients, witch will give the clue
+                print(
+"""
+As you toss the final ingredient into the cauldron, the bubbling intensifies
+and its contents rapidly change color, from light green to sky blue, deep
+violet, and finally pitch black. The cauldron begins to shake, and a high-
+pitched shriek grows in your ears. You hurridly back away when a brilliant
+flash of light temporarily blinds you. When the light fades, you see that
+the cauldron and the hut have vanished without a trace.
+
+A small scrap of paper lies on the ground in front of you, its edges charred.
+As you read its inscription, you hear a faint cackling echoing around the
+clearing - or maybe it's just the wind...
+"""
+                )
+                with open(clue, 'w') as f:
+                    f.write(
+"""
+There is much you know about this land, but some places remain hidden to you.
+Return to Bashlandia and use this command to reveal the location of the island
+where the Magic Parrot hides:
+
+ls -a
+
+Hint: use pwd to get your current location.
+"""
+                    )
+                for i in ingredients:
+                    os.remove('Cauldron/%s.txt' % i)
+                os.rmdir('./Cauldron')
+            else:
+                # Player has provided too many ingredients
+                print(
+f"""
+You move to throw the ingredients into the pot, but in a flash the witch
+appears in front of you, scowling.
+
+"I was precise in my request - bring me these ingredients three! Yet you seem
+to have brought {len(os.listdir())} items instead. The consequences for my
+brew could be catastrophic! You must discard the extra items before I allow
+you to pass."
+
+Hint: there can only be 3 files within the Cauldron directory. Make sure you
+only include the gemstone file that is 8 bytes in size.
+"""
+                )
+    else:
+        # Player hasn't found all the ingredients yet
+        print(
+"""
+You return to the hut, but there is no sign of the witch. It seems she will
+only return once you gather the dragon scale, mushroom cap, and gemstone
+(of exactly 85 bytes) and deposit them in the cauldron.
+"""
+        )
+else:
+    # First time encountering the witch
     os.mkdir(cauldron)
     print(
 """
@@ -24,51 +102,8 @@ she says, turning to look at you but still not moving her mouth.
 "Perhaps some lost things do not wish to be found. Nevertheless, I will help
 you if you can first help me. For to complete this potion, I need these 
 ingredients three: the scale of a gold dragon, the cap of a fiery mushroom,
-and the finger of a dead saint. Bring these to me, and I will help you."
+and a gemstone of precisely 85 bytes. Bring these to me, and I will help you."
 
 You blink, and the witch vanishes. The cauldron bubbles merrily.
-"""
-    )
-elif not all([Path('Cauldron/%s.txt' % i).is_file() for i in ingredients]):
-    print(
-"""
-You return to the hut, but there is no sign of the witch. It seems she will
-only return once you gather the dragon scale, mushroom cap, and dead saint's
-finger and deposit them in the cauldron.
-"""
-    )
-elif not clue.is_file():
-    print(
-"""
-As you toss the final ingredient into the cauldron, the bubbling intensifies
-and its contents rapidly change color, from light green to sky blue, deep
-violet, and finally pitch black. The cauldron begins to shake, and a high-
-pitched shriek grows in your ears. You hurridly back away when a brilliant
-flash of light temporarily blinds you. When the light fades, you see that
-the cauldron and the hut have vanished without a trace.
-
-A small scrap of paper lies on the ground in front of you, its edges charred.
-As you read its inscription, you hear a faint cackling echoing around the
-clearing - or maybe it's just the wind...
-"""
-    )
-    with open(clue, 'w') as f:
-        f.write(
-"""
-There is much you know about this land, but some places remain hidden to you.
-Return to Bashlandia and use this command to reveal the location of the island
-where the Magic Parrot hides:
-
-ls -a
-"""
-        )
-    for i in ingredients:
-        os.remove('Cauldron/%s.txt' % i)
-    os.rmdir('./Cauldron')
-else:
-    print(
-"""
-Try as you might, you cannot find the clearing where you encountered the
-witch before. Something tells you this isn't the last you've seen of her...
 """
     )
