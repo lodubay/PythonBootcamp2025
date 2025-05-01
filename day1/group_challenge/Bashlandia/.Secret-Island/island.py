157c60338a4624f8a5fff4abb10df8096c614d77
Liam Dubay <liam.dubay@gmail.com>
2025-05-01 14:43:45 -0400

    Update island.py

--- day1/group_challenge/Bashlandia/.Secret-Island/island.py
+++ day1/group_challenge/Bashlandia/.Secret-Island/island.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-rickroll = Path('./.rick')
+bashscript = Path('call_parrot.sh')
+rickroll = Path('.rick')
 
 if not rickroll.is_file():
     print(
@@ -12,30 +13,29 @@ He appears to be... dancing?
 "Hullo!" he calls out as you approach. "What brings you to my island?"
 
 "I seek a missing parrot. His magical abilities are of great renown, and he
-is sorely needed in is home kingdom," you reply.
+is sorely needed in his home kingdom," you reply.
 
 The man continues dancing. "Well, I don't know about any magic parrot,"
-he says, "but whenever I'm looking for something, I use this spell:
+he says, "but I can give you a spell that I use whenever I'm looking for
+something!"
 
-curl ascii.live/rick
+He produces a scroll and hands it to you. You take it and begin to cast
+the spell.
 
-Then whatever I need usually falls right into my lap!"
-
-You stare at the man. Could it really be so simple?
-
-Try the dancing man's spell.
+Hint: use "bash call_parrot.sh" to run the shell script. If you need to
+exit once it's running, hit Ctrl-C.
 """
     )
-    with open(rickroll, 'w') as f:
-        f.write('')
+    with open(bashscript, 'w') as f:
+        f.write('touch %s\ncurl ascii.live/rick' % rickroll)
 else:
     print(
 """
 The man laughs. "Haha! Just my little joke. Here is the spell you should
 actually use!"
 
-curl ascii.live/parrot
+curl parrot.live
 
-You roll your eyes and try the new spell.
+You roll your eyes and try the new spell. Show the instructor the result!
 """
     )
